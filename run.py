ef7c47ce457ddfd75fb00f60c546d632511ba117
ReubensIdeas <rfecollege@hotmail.co.uk>
2022-10-06 16:24:38 +0000

    created the hangman figure & basic start function

--- run.py
+++ run.py
@@ -1,3 +1,86 @@
-# Your code goes here.
+import random
+from words import word_list
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+
+def display_hangman(tries):
+    stages = [ """
+                  --------
+                  |      |
+                  |      O
+                  |     \|/
+                  |      |
+                  |     / \
+                  -
+                """,
+                """
+                  --------
+                  |      |
+                  |      O
+                  |     \|/
+                  |      |
+                  |     / 
+                  -
+                """,
+                """
+                  --------
+                  |      |
+                  |      O
+                  |     \|/
+                  |      |
+                  |     
+                  -
+                """,
+                """
+                  --------
+                  |      |
+                  |      O
+                  |     \|
+                  |      |
+                  |     
+                  -
+                """,
+                """
+                  --------
+                  |      |
+                  |      O
+                  |      |
+                  |      |
+                  |     
+                  -
+                """,
+                """
+                  --------
+                  |      |
+                  |      O
+                  |      
+                  |      
+                  |     
+                  -
+                """,
+                """
+                  --------
+                  |      |
+                  |      
+                  |      
+                  |      
+                  |     
+                  -
+                """,
+    ]
+    return stages[tries]
